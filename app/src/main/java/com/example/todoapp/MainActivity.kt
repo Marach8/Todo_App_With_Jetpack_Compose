3a6a6d59940ba311fbb4be651e4e759c6993d891
Ajah Emmanuel Nnanna <nnannamarach4@gmail.com>
2025-04-09 09:31:08 +0100

    Created a shimmer effect for loading states

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.navigation.compose.rememberNavController
+import com.example.todoapp.animated_shimmer.AnimatedShimmer
 import com.example.todoapp.bottom_navigation_bar.screens.MainScreen
 import com.example.todoapp.navigation.nav_graph.SetUpNavGraph
 import com.example.todoapp.topappbar.AppBarScreen
@@ -13,17 +15,22 @@ import com.example.todoapp.topappbar.MainViewModel
 import com.example.todoapp.ui.theme.TodoAppTheme
 
 class MainActivity : ComponentActivity() {
-//    lateinit var navController: NavHostController
-    private val viewModel : MainViewModel by viewModels()
+    //late init var navController: NavHostController
+    //val navController = rememberNavController()
+    //private val viewModel : MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TodoAppTheme {
-                AppBarScreen(viewModel = viewModel)
+                Column(){
+                    repeat(10){
+                        AnimatedShimmer()
+                    }
+                }
+                //AppBarScreen(viewModel = viewModel)
 //                MainScreen()
-//                val navController = rememberNavController()
 //                SetUpNavGraph(navController = navController)
 //                Column(
 //                    modifier = Modifier
